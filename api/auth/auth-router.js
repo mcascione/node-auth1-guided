4ccc6402822b16056a2af54441b3c8d3b1182a2f
Miranda Cascione <miranda.cascione@gmail.com>
2023-05-12 11:17:39 -0700

    bcrypt added

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,9 +1,20 @@
 const express = require("express");
-
+const bcrypt = require("bcryptjs");
 const router = express.Router();
+const User = require("../users/users-model");
 
 router.post("/register", async (req, res, next) => {
-  res.json({ message: "register working" });
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 8);
+    const newUser = { username, password: hash };
+    const result = await User.add(newUser);
+    res.status(201).json({
+        message: `nice to have you, ${result.username}`,
+    })
+  } catch (err) {
+    console.log(err);
+  }
 });
 router.post("/login", async (req, res, next) => {
   res.json({ message: "login working" });
